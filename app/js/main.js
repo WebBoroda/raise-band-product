80b609724aac2c55314101cd2441fdd202a35480
WebBoroda <webborodaweb@gmail.com>
2021-03-21 17:22:03 +0200

    add header menu

---
+++ app/js/main.js
@@ -0,0 +1,39 @@
+
+//Burger
+
+const burgerBtn = document.querySelector('.burger__btn'),
+  menu = document.querySelector('.nav');
+
+burgerBtn.addEventListener('click', () => {
+  menu.classList.toggle('active');
+  burgerBtn.classList.toggle('active');
+  document.body.classList.toggle('lock');
+});
+
+menu.addEventListener('click', (e) => {
+  if (e.target.classList.contains('nav__link')) {
+    e.preventDefault();
+    menu.classList.remove('active');
+    burgerBtn.classList.remove('active');
+    document.body.classList.remove('lock');
+  }
+
+});
+
+// End Burger
+
+// search
+
+const search = document.querySelector('.search__box'),
+  body = document.querySelector('body');
+
+search.addEventListener('click', function (e) {
+  e.stopPropagation();
+  this.classList.add('search__box_active');
+});
+
+body.addEventListener('click', function () {
+  search.classList.remove('search__box_active');
+});
+
+//# sourceMappingURL=main.js.map
