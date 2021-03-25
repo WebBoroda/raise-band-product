cad0a3db9121882ed33653ddafaffadd87819520
WebBoroda <webborodaweb@gmail.com>
2021-03-25 22:47:57 +0200

    add calc

--- src/js/main.js
+++ src/js/main.js
@@ -169,3 +169,53 @@ document.addEventListener('keydown', function (e) {
 })();
 
 //  End popup
+
+
+
+//  calc
+
+// form__input--nuber
+function change(objName, min, max, step) {
+  let obj = document.getElementById(objName);
+  let tmp = +obj.value + step;
+  if (tmp<min) tmp=min;
+  if (tmp>max) tmp=max;
+  obj.value = tmp;
+}
+
+//Calendar
+const picker = datepicker('.datepicker__element', {
+  // Event callbacks.
+  // onSelect: instance => {
+  //   // Show which date was selected.
+  //   console.log(instance.dateSelected)
+  // },
+
+  // onMonthChange: instance => {
+  //   // Show the month of the selected date.
+  //   console.log(instance.currentMonthName)
+  // },
+
+  // Customizations.
+  formatter: (input, date, instance) => {
+    // This will display the date as `1/1/2019`.
+    input.value = date.toDateString()
+    const value = date.toLocaleDateString()
+    input.value = value
+  },
+  position: 'c', // center. This can be 1 of 5 values: 'tr', 'tl', 'br', 'bl', 'c'
+  startDay: 1, // Calendar week starts on a Monday
+
+  // Settings.
+  alwaysShow: false, // Never hide the calendar.
+  dateSelected: new Date(), // Today is selected.
+  startDate: new Date(), // This month.
+  showAllDates: true, // Numbers for leading & trailing days outside the current month will show.
+
+})
+// more info https://www.npmjs.com/package/js-datepicker#installation
+//  End calendar
+
+
+
+
